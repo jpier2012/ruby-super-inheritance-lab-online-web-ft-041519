68c391060570dd1a806e619955d4a77bf3c73487
James Pier <jpier2012@gmail.com>
2019-04-23 20:30:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,8 @@
 class Student
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
+
+  def raise_hand
+    puts "Pick me!"
 end
